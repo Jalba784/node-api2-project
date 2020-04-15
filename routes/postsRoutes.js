306585ba150715365ced75ade174448514d7b58f
josepaniagua <jpanjr@gmail.com>
2020-04-15 00:22:08 -0700

    POST to posts

--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -6,8 +6,22 @@ const db = require("../data/db");
 const router = express.Router();
 
 // POST -> Create a post
-router.post("/", (req, res) => {
-  sdf;
+router.post("/", async (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    return res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  }
+  try {
+    const post = await db.insert(req.body);
+    const newlyAddedPost = await db.findById(post.id);
+    res.status(201).json(newlyAddedPost);
+  } catch (err) {
+    console.log(`Error => ${err}`);
+    res.status(500).json({
+      error: "There was an error while saving the post to the database"
+    });
+  }
 });
 // GET -> Returns array of posts
 router.get("/", async (req, res) => {
@@ -15,8 +29,9 @@ router.get("/", async (req, res) => {
     const pst = await db.find();
     res.status(200).json(pst);
   } catch (err) {
+    console.log(`Error => ${err}`);
     res.status(500).json({
-      error: "The posts information could not be retrieved",
+      error: "The posts information could not be retrieved"
     });
   }
 });
