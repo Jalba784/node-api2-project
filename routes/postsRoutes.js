e0803f919f946905bea30d432df18d15067d487b
josepaniagua <jpanjr@gmail.com>
2020-04-14 18:17:59 -0700

    latest

--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -1,10 +1,19 @@
 /* Posts Sub-Route */
 
 const express = require("express");
+const db = require("../data/db");
 
 const router = express.Router();
 
-
+// POST -> Create a post
+// GET -> Returns array of posts
+router.get("/", (req, res) => {
+    const pst = db.find();
+    res.status(200).json(pst)
+});
+// GET -> Returns specific post
+// DELETE -> Deletes post
+// PUT -> Updates post
 
 
 module.exports = router; // Export
